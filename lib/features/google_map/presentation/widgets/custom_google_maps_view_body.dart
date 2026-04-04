4024777d0815b2a852480ef4a454ecb2aaed2768
Adham-Amin <adhm.amin03@gmail.com>
2026-04-04 15:06:02 +0200

    Done App

--- lib/features/google_map/presentation/widgets/custom_google_maps_view_body.dart
+++ lib/features/google_map/presentation/widgets/custom_google_maps_view_body.dart
@@ -1,9 +1,12 @@
-import 'dart:developer';
+import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:routing_tracker/core/services/location_service.dart';
+import 'package:routing_tracker/features/google_map/domain/entities/place_entity.dart';
 import 'package:routing_tracker/features/google_map/presentation/cubit/google_map_cubit.dart';
 import 'package:routing_tracker/features/google_map/presentation/widgets/search_section.dart';
 
@@ -16,26 +19,30 @@ class CustomGoogleMapsViewBody extends StatefulWidget {
 }
 
 class _CustomGoogleMapsViewBodyState extends State<CustomGoogleMapsViewBody> {
-  late GoogleMapController googleMapController;
-  late CameraPosition initialCameraPosition;
-  late LocationServices locationService;
-  late LatLng currentLocation;
-  Set<Marker> markers = {};
-  Set<Polyline> polylines = {};
+  late GoogleMapController _mapController;
+  late LocationServices _locationService;
+  late StreamSubscription _locationSubscription;
+
+  final Set<Marker> _markers = {};
+  final Set<Polyline> _polylines = {};
+
+  LatLng? _currentLocation;
+
+  final CameraPosition _initialCameraPosition = const CameraPosition(
+    target: LatLng(28.6483557, 30.8303655),
+    zoom: 6,
+  );
 
   @override
   void initState() {
-    locationService = LocationServices();
-    initialCameraPosition = const CameraPosition(
-      target: LatLng(28.6483557, 30.8303655),
-      zoom: 6,
-    );
     super.initState();
+    _locationService = LocationServices();
   }
 
   @override
   void dispose() {
-    googleMapController.dispose();
+    _mapController.dispose();
+    _locationSubscription.cancel();
     super.dispose();
   }
 
@@ -43,94 +50,159 @@ class _CustomGoogleMapsViewBodyState extends State<CustomGoogleMapsViewBody> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        GoogleMap(
-          onMapCreated: (controller) {
-            googleMapController = controller;
-            getLocationStream();
-          },
-          initialCameraPosition: initialCameraPosition,
-          markers: markers,
-          polylines: polylines,
-        ),
-        Positioned(
-          top: 60,
-          left: 20,
-          right: 20,
-          child: SearchSection(
-            onTap: (destination) async {
-              var destinationMarker = Marker(
-                markerId: const MarkerId('destination'),
-                position: LatLng(
-                  destination.lat.toDouble(),
-                  destination.lon.toDouble(),
-                ),
-              );
-              markers.add(destinationMarker);
-              setState(() {});
-              await context.read<GoogleMapCubit>().getPolylinePoints(
-                origin: currentLocation,
-                destination: LatLng(
-                  destination.lat.toDouble(),
-                  destination.lon.toDouble(),
-                ),
-              );
-              drawPolyline(context.read<GoogleMapCubit>().polylinePoints);
-            },
-          ),
-        ),
+        _buildGoogleMap(),
+        _buildSearchSection(),
+        _buildStreamLocationButton(),
       ],
     );
   }
 
-  void drawPolyline(List<LatLng> points) {
-    polylines = {
+  // ================= UI =================
+
+  Widget _buildGoogleMap() {
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: _initialCameraPosition,
+      markers: _markers,
+      polylines: _polylines,
+    );
+  }
+
+  Widget _buildSearchSection() {
+    return Positioned(
+      top: 60,
+      left: 20,
+      right: 20,
+      child: SearchSection(onTap: _onDestinationSelected),
+    );
+  }
+
+  // ================= Map Logic =================
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+    getCurrentLocation();
+  }
+
+  Future<void> _onDestinationSelected(PlaceEntity destination) async {
+    if (_currentLocation == null) return;
+
+    final LatLng destinationLatLng = LatLng(
+      destination.lat.toDouble(),
+      destination.lon.toDouble(),
+    );
+
+    _addDestinationMarker(destinationLatLng);
+
+    await context.read<GoogleMapCubit>().getPolylinePoints(
+      origin: _currentLocation!,
+      destination: destinationLatLng,
+    );
+
+    _drawPolyline();
+
+    _moveCameraToBounds();
+  }
+
+  void _addDestinationMarker(LatLng destination) {
+    final marker = Marker(
+      markerId: const MarkerId('destination'),
+      position: destination,
+    );
+
+    _markers.add(marker);
+    setState(() {});
+  }
+
+  void _drawPolyline() {
+    _polylines.clear();
+
+    _polylines.add(
       Polyline(
         polylineId: const PolylineId('route'),
         color: Colors.blue,
         width: 5,
-        points: points,
+        points: context.read<GoogleMapCubit>().polylinePoints,
       ),
-    };
+    );
+
+    setState(() {});
+  }
+
+  void _moveCameraToBounds() {
+    final points = context.read<GoogleMapCubit>().polylinePoints;
+    var northeast = LatLng(points[0].latitude, points[0].longitude);
+    var southwest = LatLng(points[0].latitude, points[0].longitude);
+
+    for (var point in points) {
+      northeast = LatLng(
+        max(northeast.latitude, point.latitude),
+        max(northeast.longitude, point.longitude),
+      );
+      southwest = LatLng(
+        min(southwest.latitude, point.latitude),
+        min(southwest.longitude, point.longitude),
+      );
+    }
+    var bounds = LatLngBounds(northeast: northeast, southwest: southwest);
+    _mapController.animateCamera(CameraUpdate.newLatLngBounds(bounds, 64));
+  }
+
+  void _updateCurrentLocation(LocationData location) {
+    final LatLng newLocation = LatLng(location.latitude!, location.longitude!);
+
+    _currentLocation = newLocation;
+
+    _mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: newLocation, zoom: 18),
+      ),
+    );
+
+    _updateMyLocationMarker(newLocation);
+  }
+
+  void _updateMyLocationMarker(LatLng location) {
+    _markers.removeWhere((m) => m.markerId.value == 'myLocation');
+
+    _markers.add(
+      Marker(markerId: const MarkerId('myLocation'), position: location),
+    );
+
     setState(() {});
   }
 
   void getLocationStream() {
-    locationService.getLocationStream().listen((location) {
-      currentLocation = LatLng(location.latitude!, location.longitude!);
-      googleMapController.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: LatLng(location.latitude!, location.longitude!),
-            zoom: 18,
-          ),
-        ),
-      );
-      var currentMarker = Marker(
-        markerId: const MarkerId('myLocation'),
-        position: LatLng(location.latitude!, location.longitude!),
-      );
-      markers.add(currentMarker);
-      setState(() {});
+    _locationSubscription = _locationService.getLocationStream().listen((
+      location,
+    ) {
+      _updateCurrentLocation(location);
     });
   }
 
   void getCurrentLocation() {
-    locationService.getCurrentLocation().then((location) {
-      currentLocation = LatLng(location.latitude!, location.longitude!);
-      googleMapController.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: LatLng(location.latitude!, location.longitude!),
-            zoom: 18,
-          ),
-        ),
+    _locationService.getCurrentLocation().then((location) {
+      _updateCurrentLocation(location);
+
+      var marker = Marker(
+        markerId: const MarkerId('CurrentLocation'),
+        position: _currentLocation!,
       );
-      var currentMarker = Marker(
-        markerId: const MarkerId('myLocation'),
-        position: LatLng(location.latitude!, location.longitude!),
-      );
-      markers.add(currentMarker);
+
+      _markers.add(marker);
+
       setState(() {});
     });
+  }
+
+  Widget _buildStreamLocationButton() {
+    return Positioned(
+      bottom: 20,
+      right: 20,
+      child: FloatingActionButton(
+        child: const Icon(Icons.my_location),
+        onPressed: () => getLocationStream(),
+      ),
+    );
   }
 }
